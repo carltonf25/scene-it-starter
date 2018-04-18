215bdda129575665bc4891d8786dfdcdd1bc1be6
Carlton Freeman <carltonfreeman@Carltons-MacBook-Pro.local>
2018-04-17 20:06:28 -0400

    remove pointless elements and fix code spacing

--- watchlist.js
+++ watchlist.js
@@ -1,44 +1,28 @@
 $(function (){
 
-var watchListJSON = localStorage.getItem('watchlist');
-var watchlist = JSON.parse(watchListJSON);
+    var watchListJSON = localStorage.getItem('watchlist');
+    var watchlist = JSON.parse(watchListJSON);
 
-var renderMovies = function(watchlist){
-    var finalHTML = "";
-    watchlist.forEach(currentMovie => {
-        finalHTML +=
-        `
-        <div class="card movie" style="width: 18rem;">
-            <img class="card-img-top" src=${currentMovie.Poster} alt="Card image cap">
-            <div class="card-body">
-            <h5 class="card-title">${currentMovie.Title}</h5>
-            <p class="card-text">Released: ${currentMovie.Year}</p>
-            <a href="#" data-id = "${currentMovie.imdbID}" class="btn btn-primary add-btn">Add</a>
+    var renderMovies = function(watchlist){
+        var finalHTML = "";
+        watchlist.forEach(currentMovie => {
+            finalHTML +=
+            `
+            <div class="card movie" style="width: 18rem;">
+                <img class="card-img-top" src=${currentMovie.Poster} alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">${currentMovie.Title}</h5>
+                <p class="card-text">Released: ${currentMovie.Year}</p>
+                <a href="#" data-id = "${currentMovie.imdbID}" class="btn btn-primary add-btn">Add</a>
+                </div>
             </div>
-        </div>
-        `;
-    });
-    return finalHTML;
-}
+            `;
+        });
+        return finalHTML;
+    }
 
 
-var results = renderMovies(watchlist);
-$('.movies-container').html(results);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    var results = renderMovies(watchlist);
+    $('.movies-container').html(results);
 
 });
