a9ed7ae63d886fc728d36d32edc6be945847cd09
Carlton Freeman <carltonfreeman@Carltons-MacBook-Pro.local>
2018-04-17 20:02:42 -0400

    implement watchlist page

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(function(){
-
+var watchlist = [];
 
     var renderMovies = function(movieArray){
         var finalHTML = "";
@@ -11,7 +11,7 @@ $(function(){
                 <div class="card-body">
                 <h5 class="card-title">${currentMovie.Title}</h5>
                 <p class="card-text">Released: ${currentMovie.Year}</p>
-                <a href="#" class="btn btn-primary">Add</a>
+                <a href="#" data-id = "${currentMovie.imdbID}" class="btn btn-primary add-btn">Add</a>
                 </div>
             </div>
             `;
@@ -19,12 +19,31 @@ $(function(){
         return finalHTML;
     }
 
+
+
+
+
+
     $('form').submit(function(e){
         e.preventDefault();
         var results = renderMovies(movieData);
-        $('.movies-container').html(results); 
+        $('.movies-container').html(results);
     });
 
+    $('.movies-container').on('click', '.add-btn', function(e){
+        var imdbID = $(this).data('id');
+        var movie = movieData.find(function(currentMovie){
+            return currentMovie.imdbID == imdbID;
+        });
+        var watchListJSON = localStorage.getItem('watchlist');
+        var watchlist = JSON.parse(watchListJSON);
+        if (watchlist == null) {
+            watchlist = [];
+        }
+        watchlist.push(movie);
+        watchlistJSON = JSON.stringify(watchlist);
+        localStorage.setItem('watchlist', watchlistJSON);
+    });
 
 
 
---
+++ watchlist.js
@@ -0,0 +1,44 @@
+$(function (){
+
+var watchListJSON = localStorage.getItem('watchlist');
+var watchlist = JSON.parse(watchListJSON);
+
+var renderMovies = function(watchlist){
+    var finalHTML = "";
+    watchlist.forEach(currentMovie => {
+        finalHTML +=
+        `
+        <div class="card movie" style="width: 18rem;">
+            <img class="card-img-top" src=${currentMovie.Poster} alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">${currentMovie.Title}</h5>
+            <p class="card-text">Released: ${currentMovie.Year}</p>
+            <a href="#" data-id = "${currentMovie.imdbID}" class="btn btn-primary add-btn">Add</a>
+            </div>
+        </div>
+        `;
+    });
+    return finalHTML;
+}
+
+
+var results = renderMovies(watchlist);
+$('.movies-container').html(results);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
